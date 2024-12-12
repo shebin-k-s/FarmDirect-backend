2e7b9318741ef4c44dbd0ea61ede4e07878f2c0d
shebin ks <shebinks01@gmail.com>
2024-12-13 00:11:27 +0530

    included chat feature

---
+++ controllers/messageController.js
@@ -0,0 +1,135 @@
+import Message from "../models/messageModel.js"
+import User from "../models/userModel.js"
+import { DateTime } from "luxon"
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { profileId, message } = req.body
+        const sender = req.user.userId
+        console.log(sender);
+        console.log(profileId);
+        let receiverExist = await User.findById(profileId)
+
+        if (!receiverExist) {
+            return res.status(400).json({ message: "Receiver doesn't exist" })
+        }
+
+        const newMessage = new Message({
+            sender,
+            receiver: profileId,
+            message
+        })
+
+        const chatNameSpace = req.app.get('chatNameSpace');
+        const connectedUsers = req.app.get('connectedUsers');
+        if (connectedUsers.has(profileId)) {
+            const receiverData = connectedUsers.get(profileId);
+            if (receiverData.interactingWith === sender) {
+                newMessage.status = 'seen';
+            } else {
+                newMessage.status = 'received';
+            }
+        } else {
+            newMessage.status = 'sent';
+        }
+        chatNameSpace.to(receiverData.socketId).emit('newMessage', newMessage);
+        await newMessage.save()
+        return res.status(201).json(newMessage);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+export const getMessagedProfiles = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        const messages = await Message.find({
+            $or: [{ sender: userId }, { receiver: userId }]
+        }).sort({ sendAt: -1 });
+
+        const uniqueUsers = {};
+        messages.forEach(message => {
+            const otherUserId = message.sender.toString() !== userId.toString() ? message.sender.toString() : message.receiver.toString();
+            if (!uniqueUsers[otherUserId]) {
+
+                uniqueUsers[otherUserId] = {
+                    userId: otherUserId,
+                    latestMessage: message.message,
+                    latestMessageSendAt: message.sendAt,
+                    messageStatus: message.status,
+                    unreadCount: 0
+                };
+            }
+            if (message.receiver.toString() === userId.toString() && message.status != 'seen') {
+                uniqueUsers[otherUserId].unreadCount++;
+            }
+        });
+
+        const userIdsArray = Object.keys(uniqueUsers);
+        const profiles = await User.find(
+            {
+                _id: { $in: userIdsArray }
+            },
+            { username: 1, profilePic: 1 }
+        );
+
+        const result = userIdsArray.map(id => {
+            const profile = profiles.find(profile => profile._id.toString() === id);
+            return {
+                profile,
+                latestMessage: uniqueUsers[id].latestMessage,
+                latestMessageSendAt: uniqueUsers[id].latestMessageSendAt,
+                messageStatus: uniqueUsers[id].messageStatus,
+                unreadCount: uniqueUsers[id].unreadCount
+            };
+        });
+
+        res.status(200).json({ messagedProfiles: result });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+export const getConversation = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const { profileId } = req.params;
+
+        const recipientExist = await User.findById(profileId);
+        if (!recipientExist) {
+            return res.status(400).json({ message: "The user you're trying to converse with doesn't exist" });
+        }
+
+        const messages = await Message.find({
+            $or: [
+                { sender: userId, receiver: profileId },
+                { sender: profileId, receiver: userId }
+            ]
+        }).sort({ sendAt: 1 });
+
+        const groupedMessages = messages.reduce((acc, message) => {
+            const istDate = DateTime.fromJSDate(message.sendAt, { zone: 'Asia/Kolkata' }).toISODate();
+
+            if (!acc[istDate]) {
+                acc[istDate] = [];
+            }
+            acc[istDate].push(message);
+            return acc;
+        }, {});
+
+        const conversationByDate = Object.keys(groupedMessages).map(date => ({
+            date,
+            messages: groupedMessages[date]
+        })).reverse();
+
+        return res.status(200).json({
+            conversation: conversationByDate,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error: error.message });
+    }
+};
--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express'
+import { getConversation, getMessagedProfiles, sendMessage } from "../controllers/messageController.js"
 
 const router = express.Router()
 
-router.route('/')
 
+router.route('/send')
+    .post(sendMessage)
+
+router.route('/profiles')
+    .get(getMessagedProfiles)
+
+router.route('/conversation/:profileId')
+    .get(getConversation)
 
 
 export default router
